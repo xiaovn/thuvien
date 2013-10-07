bb0b1816c235a079e2d8d899722e571278c8eb66
NguyenHoc <nguyenhoc@xiao.vn>
2013-10-07 10:25:06 +0700

    Commit menu Library Lần 2 Nguyễn Học 7/10/2013

--- template/thuviengiaoduc/nav.php
+++ template/thuviengiaoduc/nav.php
@@ -19,7 +19,6 @@
                 <div class="col_sub">
                     <ul>
                         <li><a title="#" href="#">Bài giảng</a></li>
-
                     </ul>
                 </div>
                 <div class="col_sub">
@@ -32,28 +31,7 @@
             <div id="arrow"></div>
         </li>
         <li class="mn" title="">
-            <a href="#">Cấp học</a>
-            <div class="sub_mn shadow" style="width: 423px; display: none; left: auto; left:-1px">
-                <div class="col_sub">
-                    <ul>
-                        <li><a title="#" href="#">Cấp 1</a></li>
-                    </ul>
-                </div>
-                <div class="col_sub">
-                    <ul>
-                        <li><a title="#" href="#">Cấp 2</a></li>
-                    </ul>
-                </div>
-                <div class="col_sub">
-                    <ul>
-                        <li><a title="#" href="#">Cấp 3</a></li>
-                    </ul>
-                </div>
-            </div>
-            <div id="arrow"></div>
-        </li>
-        <li class="mn" title="">
-            <a href="#">Môn/Chuyên ngành</a>
+            <a href="#">Tài liệu tham khảo</a>
             <div class="sub_mn shadow" style="width: 423px; display: none; left: auto; left:-1px">
                 <div class="col_sub">
                     <ul>
@@ -92,7 +70,7 @@
                 </div>
                 <div class="col_sub">
                     <ul>
-                        <li><a title="#" href="#">Tiếng Trung Quốc</a></li>
+                        <li><a title="#" href="#">Tiếng Trung</a></li>
                     </ul>
                 </div>
                 <div class="col_sub">
@@ -162,28 +140,12 @@
                 </div>
                 <div class="col_sub">
                     <ul>
-                        <li><a title="#" href="#">Hướng nghiệp</a></li>
-                    </ul>
-                </div>
-                <div class="col_sub">
-                    <ul>
                         <li><a title="#" href="#">Tâm lý học</a></li>
                     </ul>
                 </div>
-            </div>
-            <div id="arrow"></div>
-        </li>
-        <li class="mn" title="">
-            <a href="#">Loại sách</a>
-            <div class="sub_mn shadow" style="width: 423px; display: none; left: auto; left:-1px">
                 <div class="col_sub">
                     <ul>
-                        <li><a title="#" href="#">Sách mềm</a></li>
-                    </ul>
-                </div>
-                <div class="col_sub">
-                    <ul>
-                        <li><a title="#" href="#">Audio</a></li>
+                        <li><a title="#" href="#">Hướng nghiệp</a></li>
                     </ul>
                 </div>
             </div>
@@ -196,8 +158,31 @@
                     <ul>
                         <li rel="sub_2"><a title="#" href="#">Đề thi đại học</a></li>
                         <li rel="sub_3" class="iconSub_3">
-                            <a class="a_sub_3" title="#" href="#">Đề thi tốt nghiệp</a>
+                            <a class="a_sub_3" title="#" href="#">Đề thi học sinh giỏi</a>
                             <div class="col_sub sub_3">
+                                <ul>
+                                    <li><a title="#" href="#">Cấp trường</a></li>
+                                </ul>
+                                <ul>
+                                    <li><a title="#" href="#">Cấp thành phố</a></li>
+                                </ul>
+                                <ul>
+                                    <li><a title="#" href="#">Cấp huyện</a></li>
+                                </ul>
+                                <ul>
+                                    <li><a title="#" href="#">Cấp tỉnh</a></li>
+                                </ul>
+                                <ul>
+                                    <li><a title="#" href="#">Quốc gia</a></li>
+                                </ul>
+                                <ul>
+                                    <li><a title="#" href="#">Quốc tế</a></li>
+                                </ul>
+                            </div>
+                        </li>
+                        <li rel="sub_4" class="iconSub_4">
+                            <a class="a_sub_4" title="#" href="#">Đề thi tốt nghiệp</a>
+                            <div class="col_sub sub_4">
                                 <ul>
                                     <li><a title="#" href="#">Ngữ văn</a></li>
                                 </ul>
@@ -230,29 +215,27 @@
                                 </ul>
                             </div>
                         </li>
-                        <li rel="sub_3" class="iconSub_3">
-                            <a class="a_sub_3" title="#" href="#">Đề thi học sinh giỏi</a>
-                            <div class="col_sub sub_3">
-                                <ul>
-                                    <li><a title="#" href="#">Cấp trường</a></li>
-                                </ul>
-                                <ul>
-                                    <li><a title="#" href="#">Cấp thành phố</a></li>
-                                </ul>
-                                <ul>
-                                    <li><a title="#" href="#">Cấp huyện</a></li>
-                                </ul>
-                                <ul>
-                                    <li><a title="#" href="#">Cấp tỉnh</a></li>
-                                </ul>
-                                <ul>
-                                    <li><a title="#" href="#">Quốc gia</a></li>
-                                </ul>
-                                <ul>
-                                    <li><a title="#" href="#">Quốc tế</a></li>
-                                </ul>
-                            </div>
-                        </li>
+                    </ul>
+                </div>
+            </div>
+            <div id="arrow"></div>
+        </li>
+        <li title="" class="mn ">
+            <a href="javascript:;">Cấp học</a>
+            <div style="width:210px;" class="sub_mn shadow">
+                <div class="col_sub last_des">
+                    <ul>
+                        <li><a title="" href="#">Cấp 1</a></li>
+                    </ul>
+                </div>
+                <div class="col_sub last_des">
+                    <ul>
+                        <li><a title="" href="#">Cấp 2</a></li>
+                    </ul>
+                </div>
+                <div class="col_sub last_des">
+                    <ul>
+                        <li><a title="" href="#">Cấp 3</a></li>
                     </ul>
                 </div>
             </div>
@@ -268,20 +251,52 @@
                             <div class="col_sub sub_3" style="width: 360px !important;">
                                 <ul style="float: left;width: 170px;">
 
-                                    <li><a title="S" href="#">Toán giải tích</a></li>
+                                    <li><a title="S" href="#">Hình học</a></li>
 
                                 </ul>
                                 <ul style="float: right;width: 170px;">
-                                    <li><a title="S" href="#">Toán giải tích</a></li>
+                                    <li><a title="S" href="#">Đại số</a></li>
                                 </ul>
                             </div>
                         </li>
-                        <li rel="sub_2"><a title="#" href="#">Vật lý</a></li>
+                        <li rel="sub_4"><a title="#" href="#">Thể dục</a></li>
+                        <li rel="sub_5"><a title="#" href="#">Âm nhạc</a></li>
+                        <li rel="sub_6"><a title="#" href="#">Mỹ thuật</a></li>
+                        <li rel="sub_7"><a title="#" href="#">Tin học</a></li>
+                        <li rel="sub_8"><a title="#" href="#">Tiếng dân tộc</a></li>
+                        <li rel="sub_9"><a title="#" href="#">Tiếng Anh</a></li>
+                        <li rel="sub_10"><a title="#" href="#">Tiếng Pháp</a></li>
+                        <li rel="sub_11"><a title="#" href="#">Tiếng Trung</a></li>
+                        <li rel="sub_12"><a title="#" href="#">Tiếng Nga</a></li>
+                        <li rel="sub_13"><a title="#" href="#">Ngoại ngữ khác</a></li>
+                        <li rel="sub_14"><a title="#" href="#">Ngữ văn</a></li>
+                        <li rel="sub_15"><a title="#" href="#">Lịch sử</a></li>
+                        <li rel="sub_16"><a title="#" href="#">Địa lý</a></li>
+                        <li rel="sub_17"><a title="#" href="#">Vật lý</a></li>
+                        <li rel="sub_18"><a title="#" href="#">Hóa học</a></li>
+                        <li rel="sub_19"><a title="#" href="#">Sinh học</a></li>
+                        <li rel="sub_20"><a title="#" href="#">Giáo dục công dân</a></li>
+                        <li rel="sub_21"><a title="#" href="#">Giáo dục quốc phòng</a></li>
+                        <li rel="sub_22"><a title="#" href="#">Công nghệ</a></li>
+                        <li rel="sub_23"><a title="#" href="#">Tin học ứng dụng</a></li>
+                        <li rel="sub_24"><a title="#" href="#">Hướng nghiệp</a></li>
                     </ul>
                 </div>
             </div>
             <div id="arrow"></div>
         </li>
+        <li title="" class="mn ">
+            <a href="#">Đa phương tiện</a>
+            <div style="width:210px;left:auto;right:-40px" class="sub_mn shadow">
+                <div class="col_sub">
+                    <ul>
+                        <li><a title="#" href="#">Sách nói - audio</a></li>
+                    </ul>
+                </div>
+            </div>
+            <div id="arrow"></div>
+        </li>
+
         <li title="" class="no_arown mn">
             <a href="">Diễn đàn</a>
         </li>
